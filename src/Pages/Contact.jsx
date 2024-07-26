63a0d33a485d9be1e37b3a76ce13c6ff4dde786d
Subhan Qamar <subiq1917@gmail.com>
2024-07-26 18:19:25 +0500

    UI changes

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,27 +6,27 @@ const Contact = () => {
     <motion.div initial={{translateX:'100%'}} animate={{translateX:0}} exit={{translateX:'-100%'}}  transition={{ease:'backInOut',duration:0.5}} className='relative  h-full flex flex-wrap px-8 lg:px-0 justify-center items-center'>
         <div className='relative ballon1 ballon1-animation z-0'>
           <Link to="https://instagram.com/subhan_qamar_2002/?igsh=MXRkYWF5YWtzY2IvNA==" target='_blank'>
-            <i className='absolute bi bi-instagram text-pink-700 bg-white dark:bg-black rounded-lg text-4xl bottom-0 right-0 z-10'></i>
+            <i className='absolute bi bi-instagram text-pink-700 bg-white rounded-lg text-4xl bottom-0 right-0 z-10'></i>
           </Link>
         </div>
         <div className='ballon2 relative ballon1-animation z-0'>
           <Link to="https://github.com/Subhan-hub1917" target='_blank'>
-            <i className='absolute bi bi-github  bg-white dark:bg-black rounded-full text-4xl bottom-0 right-14 z-10'></i>
+            <i className='absolute bi bi-github  bg-white rounded-full text-4xl bottom-0 right-14 z-10'></i>
           </Link>
         </div>
         <div className='block md:hidden ballon4 pb-10  relative ballon1-animation z-0'>
           <Link to="https://wa.me/+923420443066" target='_blank'>
-            <i className='absolute bi bi-whatsapp text-green-600 bg-white  dark:bg-black rounded-full text-4xl bottom-0 right-0 z-10'></i>
+            <i className='absolute bi bi-whatsapp text-green-600 bg-white  rounded-full text-4xl bottom-0 right-0 z-10'></i>
           </Link>
         </div>
         <div className='ballon3  relative ballon1-animation z-0'>
           <Link to="https://www.linkedin.com/in/subhan-qamar/965946282?utm_source=share&utm_compaign=share_via&utm_content=profile&utm_medium=adriod_app" target='_blank'>
-            <i className='absolute bi bi-linkedin text-blue-600  dark:bg-black bg-white text-4xl bottom-0 right-14 z-10'></i>
+            <i className='absolute bi bi-linkedin text-blue-600  bg-white text-4xl bottom-0 right-14 z-10'></i>
           </Link>
         </div>
         <div className='hidden md:block ballon4  relative ballon1-animation z-0'>
           <Link to="https://wa.me/+923420443066" target='_blank'>
-            <i className='absolute bi bi-whatsapp text-green-600 bg-white  dark:bg-black rounded-full text-4xl bottom-0 right-0 z-10'></i>
+            <i className='absolute bi bi-whatsapp text-green-600 bg-white  rounded-full text-4xl bottom-0 right-0 z-10'></i>
           </Link>
         </div>
     </motion.div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar=({active,setActive})=> {
     <nav className='container md:hidden '>
       {/* mobile */}
           <ul  className=' py-2 flex flex-wrap items-center justify-center text-lg font-medium space-x-3 outfit-regular'>
-            <NavLink to='/' onClick={()=>setActive('/')}><li className={`${active=='/' ? 'border border-gray-700 bg-black px-2 text-white dark:bg-white dark:text-black rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'} `}>Home</li></NavLink>
-            <NavLink to='/Skills' onClick={()=>setActive('/Skills')}><li className={`${active=='/Skills' ? 'border border-gray-700 bg-black px-2 text-white dark:bg-white dark:text-black rounded-full' : 'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Skills</li></NavLink>
-            <NavLink to='/Projects' onClick={()=>setActive('/Projects')}><li className={`${active=='/Projects'?'border border-gray-700 bg-black px-2 text-white dark:bg-white dark:text-black rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Projects</li></NavLink>
-            <NavLink to='/Contact' onClick={()=>setActive('/Contact')}><li className={`${active=='/Contact' ? 'border border-gray-700 bg-black px-2 text-white dark:bg-white dark:text-black rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Contact</li></NavLink>
+            <NavLink to='/' onClick={()=>setActive('/')}><li className={`${active=='/' ? 'border border-gray-700 bg-black px-2 text-white rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'} `}>Home</li></NavLink>
+            <NavLink to='/Skills' onClick={()=>setActive('/Skills')}><li className={`${active=='/Skills' ? 'border border-gray-700 bg-black px-2 text-white rounded-full' : 'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Skills</li></NavLink>
+            <NavLink to='/Projects' onClick={()=>setActive('/Projects')}><li className={`${active=='/Projects'?'border border-gray-700 bg-black px-2 text-white rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Projects</li></NavLink>
+            <NavLink to='/Contact' onClick={()=>setActive('/Contact')}><li className={`${active=='/Contact' ? 'border border-gray-700 bg-black px-2 text-white rounded-full':'border border-gray-700 hover:bg-black px-2 hover:text-white rounded-full'}`}>Contact</li></NavLink>
           </ul>
       </nav>
       <nav className='hidden md:flex  items-center justify-center sticky'>
